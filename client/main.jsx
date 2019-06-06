9c4d8660376be887285fa1454812325cedcca278
Candice <candice@aytech.ca>
2019-06-05 18:03:23 -0700

    Reorganized project folders and installed redux packages

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import App from '/imports/ui/App'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from '/imports/ui/App';
+import reducers from '../imports/ui/reducers';
+import '../imports/ui/css/main.css';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  render(
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>,
+    document.getElementById('react-target'),
+  );
 });
--- imports/ui/components/Info.jsx
+++ imports/ui/components/Info.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-import Links from '../api/links';
+import Links from '../../api/links';
 
 class Info extends Component {
   render() {
