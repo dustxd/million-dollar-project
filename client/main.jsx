0d49a3e0d61702b5e43bafedbbd352c292af0c8f
Candice <candice@aytech.ca>
2019-06-21 15:11:21 -0700

    Installed and did initial set up for redux-thunk

--- client/main.jsx
+++ client/main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import thunk from 'redux-thunk';
 
 import App from '../imports/ui/App';
-import reducers from '../imports/ui/reducers';
+import RootReducer from '../imports/ui/reducers';
 import './main.css';
 import customizedThemes from '../imports/ui/css/customMuiStyles';
 
@@ -14,7 +15,7 @@ const theme = createMuiTheme(customizedThemes);
 
 Meteor.startup(() => {
   render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(RootReducer, applyMiddleware(thunk))}>
       <MuiThemeProvider theme={theme}>
         <App />
       </MuiThemeProvider>
