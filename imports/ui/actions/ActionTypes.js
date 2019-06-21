0c178aae2be7a93ed0189aad8f91329663d46309
Candice <candice@aytech.ca>
2019-06-21 15:11:12 -0700

    Added action types file

---
+++ imports/ui/actions/ActionTypes.js
@@ -0,0 +1,8 @@
+// api calls - request
+export const LOGIN_USER_REQUEST = 'LOGIN_USER_REQUEST';
+
+// api calls - success
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
+
+// api calls - failure
+export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE';
