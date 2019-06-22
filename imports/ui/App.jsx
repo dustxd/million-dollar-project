6ea0466e96b5bbf76343ba08c28a713a027eaa1f
Candice <candice@aytech.ca>
2019-06-21 17:35:05 -0700

    Added bindActionCreators to App.jsx so that all components can make API calls without the need to connect to redux store

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import * as Actions from './actions/index';
 import CoreRoute from './components/core/CoreRoute';
 import Login from './components/authentication/LoginPage';
 import Overview from './components/overviewPage/Overview';
 import Spread from './components/journalPage/Spread';
 
-const App = () => (
+const App = props => (
   <BrowserRouter>
     <div className="App">
       <Switch>
-        <Route exact path="/login" name="login" render={() => <Login />} />
-        <CoreRoute exact path="/overview" name="overview" render={() => <Overview />} />
-        <CoreRoute exact path="/spread" name="spread" render={() => <Spread />} />
+        <Route exact path="/login" name="login" render={() => <Login {...props} />} />
+        <CoreRoute exact path="/overview" name="overview" render={() => <Overview {...props} />} />
+        <CoreRoute exact path="/spread" name="spread" render={() => <Spread {...props} />} />
       </Switch>
     </div>
   </BrowserRouter>
 );
 
-export default App;
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(Actions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
