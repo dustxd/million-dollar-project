8cb467ecbe1ed00d645c18e7f04f1bb6d6f0cd58
Candice <candice@aytech.ca>
2019-06-21 23:04:29 -0700

    Selective rendering linear progress bar or spread page depending on loading state

--- imports/ui/components/journalPage/Spread.jsx
+++ imports/ui/components/journalPage/Spread.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { LinearProgress } from '@material-ui/core';
+
 import Page from './Page';
 import '../../css/journalPage/Spread.css';
 
 class Spread extends Component {
-	render() {
-		return (
-			<div className="spread-container">
-				<div className="spread grid-container">
-					<Page page="left" />
-					<Page page="right" />
-				</div>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const { loading } = this.props;
+
+    return (
+      loading
+        ? <LinearProgress />
+        : (
+          <div className="spread-container">
+            <div className="spread grid-container">
+              <Page page="left" />
+              <Page page="right" />
+            </div>
+          </div>
+        )
+    );
+  }
 }
 
 export default Spread;
