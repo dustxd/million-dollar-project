0faaaef9458f9a505100f3b9a0cc05c2923dbbb8
Candice <candice@aytech.ca>
2019-07-06 00:07:31 -0700

    Added new line at the end of the file

--- imports/api/entries.js
+++ imports/api/entries.js
@@ -23,6 +23,7 @@ Meteor.methods({
       type,
       createdAt,
     } = newEntry;
+
     // // Make sure the user is logged in before inserting a task
     // if (!Meteor.userId()) {
     //   throw new Meteor.Error('not-authorized');
