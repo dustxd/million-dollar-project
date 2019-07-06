65bc16bcafa6603274fc0afcc144017ddbe5cd72
Sarah Ng <sarah.ng26@gmail.com>
2019-07-06 12:50:17 -0700

    Added list items with bullet points to Entry

--- imports/ui/components/journalPage/Entry.jsx
+++ imports/ui/components/journalPage/Entry.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import LineItem from './LineItem';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
 
 class Entry extends Component {
+	constructor(props) {
+		
+	}
 	render() {
 		return (
 			<div>
-				<span className="header">{this.props.header}</span>
+			<Typography
+          variant="h2"
+        >{this.props.header}</Typography>
 				<div className="entry">
-					<li>Item 1</li>
-					<li>Item 2</li>
-					<li>Item 3</li>
+				<List component="nav">
+					<LineItem bulletType='toDo' content='Need to do this'/>
+					<LineItem bulletType='toDoCompleted' content='Done with this'/>
+					<LineItem bulletType='toDoScheduled' content='Scheduled'/>
+					<LineItem bulletType='toDoMigrated' content='Migrated this'/>
+					<LineItem bulletType='event' content='An event'/>
+					<LineItem bulletType='note' content='A memorable note'/>
+					</List>
 				</div>
 			</div>
 		);
