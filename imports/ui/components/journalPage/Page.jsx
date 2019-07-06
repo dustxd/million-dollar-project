65b1b7099783dc29852dde6fd5ce86db7570dbd0
Sarah Ng <sarah.ng26@gmail.com>
2019-07-06 12:50:01 -0700

    Added material ui styling to Page

--- imports/ui/components/journalPage/Page.jsx
+++ imports/ui/components/journalPage/Page.jsx
@@ -1,34 +1,60 @@
 import React, { Component } from 'react';
+//import { Grid, Paper } from '@material-ui/core';
 import Entry from './Entry';
 import ButtonNewPage from './ButtonNewPage';
+import { withStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
+
+const styles = {
+  leftPage: {
+    padding: '1em',
+    paddingRight: '3em',
+  },
+  rightPage: {
+    padding: '1em',
+    paddingLeft: '3em',
+  }
+};
 
 class Page extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
+  
   render() {
-    const { page } = this.props;
-
-    if (page === 'left') {
+    const { classes } = this.props;
+    switch (this.props.page) {
+      case "right":
       return (
-        <div className="grid-item left-page">
-          <Entry header="Grocery list" />
-          <Entry header="Snack list" />
+          <div>
+        {/* <Grid> */}
+          <div className={classes.rightPage}>
+          <Entry header="Jun 8 | Sat"/>
+          {/* <ButtonNewPage /> */}
+          </div>
+          
+        {/* </Grid>   */}
+        </div>
+      )
+    case "left":
+      return (
+          <div>
+        {/* <Grid> */}
+       <div className={classes.rightPage}>
+          <Entry header="A list" />
+        </div>
         </div>
       );
-    }
-
-    return (
+    
+    default:
+      return (
       <div className="grid-item right-page">
+      <div>
         <Entry header="Jun 8 | Sat" />
         <ButtonNewPage />
       </div>
+      </div>
     );
   }
+  
 }
 
-export default Page;
+}
+export default withStyles(styles)(Page);
