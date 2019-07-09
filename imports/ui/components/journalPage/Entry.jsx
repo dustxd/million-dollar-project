51d8a7549e0ac1597cf337320f2666428541c17c
Candice <candice@aytech.ca>
2019-07-09 02:49:51 -0700

    Fixed eslint errors in Entry.jsx

--- imports/ui/components/journalPage/Entry.jsx
+++ imports/ui/components/journalPage/Entry.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { 
-	Icon, 
-	IconButton, 
-	List,  
-	Typography, 
+import {
+  Icon,
+  IconButton,
+  List,
+  Typography,
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -35,17 +35,16 @@ class Entry extends Component {
           </IconButton>
         </div>
         <List component="nav">
-					<LineItem bulletType='toDo' content='Need to do this'/>
-					<LineItem bulletType='toDoCompleted' content='Done with this'/>
-					<LineItem bulletType='toDoScheduled' content='Scheduled'/>
-					<LineItem bulletType='toDoMigrated' content='Migrated this'/>
-					<LineItem bulletType='event' content='An event'/>
-					<LineItem bulletType='note' content='A memorable note'/>
-				</List>
+          <LineItem bulletType='toDo' content='Need to do this'/>
+          <LineItem bulletType='toDoCompleted' content='Done with this'/>
+          <LineItem bulletType='toDoScheduled' content='Scheduled'/>
+          <LineItem bulletType='toDoMigrated' content='Migrated this'/>
+          <LineItem bulletType='event' content='An event'/>
+          <LineItem bulletType='note' content='A memorable note'/>
+        </List>
       </div>
     );
   }
 }
 
 export default withStyles(styles)(Entry);
-
