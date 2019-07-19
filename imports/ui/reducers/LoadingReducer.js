d28affcb71528cef34e3079eea7e752b53d660e1
Candice <candice@aytech.ca>
2019-07-19 16:16:03 -0700

    Added add and update actions to loading reducer

--- imports/ui/reducers/LoadingReducer.js
+++ imports/ui/reducers/LoadingReducer.js
@@ -3,9 +3,15 @@ import * as types from '../actions/ActionTypes';
 const LoadingReducer = (loading = 0, action) => {
   switch (action.type) {
     case types.LOGIN_USER_REQUEST:
+    case types.ADD_RESOURCE_REQUEST:
+    case types.UPDATE_RESOURCE_REQUEST:
       return loading + 1;
     case types.LOGIN_USER_SUCCESS:
     case types.LOGIN_USER_FAILURE:
+    case types.ADD_RESOURCE_SUCCESS:
+    case types.ADD_RESOURCE_FAILURE:
+    case types.UPDATE_RESOURCE_SUCCESS:
+    case types.UPDATE_RESOURCE_FAILURE:
       return loading - 1;
     case types.LOGOUT_USER:
       return 0;
