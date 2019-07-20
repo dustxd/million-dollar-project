0b4574745ead7ace7adcdfc6654d8258f270f830
Candice <candice@aytech.ca>
2019-07-19 19:03:28 -0700

    Added delete action to loading reducer

--- imports/ui/reducers/LoadingReducer.js
+++ imports/ui/reducers/LoadingReducer.js
@@ -5,6 +5,7 @@ const LoadingReducer = (loading = 0, action) => {
     case types.LOGIN_USER_REQUEST:
     case types.ADD_RESOURCE_REQUEST:
     case types.UPDATE_RESOURCE_REQUEST:
+    case types.DELETE_RESOURCE_REQUEST:
       return loading + 1;
     case types.LOGIN_USER_SUCCESS:
     case types.LOGIN_USER_FAILURE:
@@ -12,6 +13,8 @@ const LoadingReducer = (loading = 0, action) => {
     case types.ADD_RESOURCE_FAILURE:
     case types.UPDATE_RESOURCE_SUCCESS:
     case types.UPDATE_RESOURCE_FAILURE:
+    case types.DELETE_RESOURCE_SUCCESS:
+    case types.DELETE_RESOURCE_FAILURE:
       return loading - 1;
     case types.LOGOUT_USER:
       return 0;
