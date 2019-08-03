7b00398446089a6956f198d7709a81cefd4958c6
Sarah Ng <sarah.ng26@gmail.com>
2019-08-03 01:29:31 -0700

    Added dummy quotes to overview and constant quote file

--- imports/ui/components/overviewPage/Overview.jsx
+++ imports/ui/components/overviewPage/Overview.jsx
@@ -3,25 +3,37 @@ import { LinearProgress, Typography } from '@material-ui/core';
 
 import OverviewActions from './OverviewActions';
 import SearchBar from './SearchBar';
+import INSPIRATIONAL_QUOTES from '../../constants/quotes';
 import '../../css/overviewPage/Overview.css';
+
+
+
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      quotes: INSPIRATIONAL_QUOTES.INSPIRATIONAL_QUOTES,
     };
   }
+  
 
   render() {
-    const { loading, user, actions } = this.props;
-
+    const { loading, user, actions } = this.props;  
+    
+  
+    //const quotes = INSPIRATIONAL_QUOTES;
+    console.log(this.state.quotes);
+    
+    var quote = this.state.quotes[Math.floor(Math.random()*this.state.quotes.length)];
+    console.log(quote);
     return (
       loading
         ? <LinearProgress />
         : (
           <div className="overview-container">
             <Typography variant="h4">Hi, {user && user.profile.firstName}!</Typography>
+            <Typography variant="h6">{quote}</Typography>
             <SearchBar user={user} />
             <OverviewActions actions={actions} />
           </div>
---
+++ imports/ui/constants/quotes.js
@@ -0,0 +1,1 @@
+export const INSPIRATIONAL_QUOTES = ["Quote 1", "Quote 2", "Quote 3", "Quote 4"];
