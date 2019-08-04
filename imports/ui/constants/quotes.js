bb0f0910641b43482870c510ddaccefdc56df0a3
Sarah Ng <sarah.ng26@gmail.com>
2019-08-03 23:44:52 -0700

    Switched dummy quotes to real quotes by real people

--- imports/ui/constants/quotes.js
+++ imports/ui/constants/quotes.js
@@ -1,1 +1,11 @@
-export const INSPIRATIONAL_QUOTES = ["Quote 1", "Quote 2", "Quote 3", "Quote 4"];
+export default INSPIRATIONAL_QUOTES = [
+  "The secret of getting ahead is getting started.",
+  "Anyone who has never made a mistake has never tried anything new.",
+  "If you spend too much time thinking about a thing, you’ll never get it done.",
+  "Success is a lousy teacher. It seduces smart people into thinking they can’t lose.",
+  "If you have to eat two frogs, eat the ugliest one first.",
+  "Tomorrow becomes never. No matter how small the task, take the first step now!",
+  "Never mistake motion for action.",
+  "In life, people tend to wait for good things to come to them. And by waiting, they miss out.",
+];
+
