d3eb845614b266d370c6a70609a6a3b314a4d697
Sarah Ng <sarah.ng26@gmail.com>
2019-08-03 23:46:52 -0700

    Added time-dependent greeting text and quote to overview page, adjested padding

--- imports/ui/components/overviewPage/Overview.jsx
+++ imports/ui/components/overviewPage/Overview.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import { LinearProgress, Typography } from '@material-ui/core';
-
+import moment from 'moment';
 import OverviewActions from './OverviewActions';
 import SearchBar from './SearchBar';
-import INSPIRATIONAL_QUOTES from '../../constants/quotes';
+import INSPIRATIONAL_QUOTES from '../../constants/Quotes';
 import '../../css/overviewPage/Overview.css';
-
-
-
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes: INSPIRATIONAL_QUOTES.INSPIRATIONAL_QUOTES,
+      quotes: INSPIRATIONAL_QUOTES,
     };
   }
-  
+
+  greetingText = () => {
+    const now = moment();
+    const currentHour = now.hour();
+      if (currentHour >= 5 && currentHour < 12) return "Good morning,"
+      else if (currentHour >= 12 && currentHour < 17) return "Good afternoon,"
+      else return "Good evening,"
+  }
 
   render() {
     const { loading, user, actions } = this.props;  
-    
-  
-    //const quotes = INSPIRATIONAL_QUOTES;
-    console.log(this.state.quotes);
-    
+    const greeting = this.greetingText();
     var quote = this.state.quotes[Math.floor(Math.random()*this.state.quotes.length)];
-    console.log(quote);
+  
     return (
       loading
         ? <LinearProgress />
         : (
           <div className="overview-container">
-            <Typography variant="h4">Hi, {user && user.profile.firstName}!</Typography>
-            <Typography variant="h6">{quote}</Typography>
+            <Typography variant="h4">{greeting} {user && user.profile.firstName}.</Typography>
             <SearchBar user={user} />
             <OverviewActions actions={actions} />
+            <Typography variant="overline">"{quote}"</Typography>
           </div>
         )
     );
