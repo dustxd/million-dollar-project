576ca23c3267ceb3b8197e0cf9a96d8f0d1878e3
Candice <candice@aytech.ca>
2019-08-09 21:40:59 -0700

    Updated getDisplayedEntryId to use entries if the prop is available to use

--- imports/ui/components/journalPage/Page.jsx
+++ imports/ui/components/journalPage/Page.jsx
@@ -53,21 +53,22 @@ class Page extends Component {
   }
 
   getDisplayedEntryId = (redirectedEntryId) => {
-    const { defaultEntries, position } = this.props;
+    const { entries, defaultEntries, position } = this.props;
+    const inputEntries = entries || defaultEntries;
 
     if (redirectedEntryId) {
       return redirectedEntryId;
     }
 
-    if (defaultEntries && defaultEntries.length > 0) {
+    if (inputEntries && inputEntries.length > 0) {
       // This handles Spread View:
       // 1. If there is more than one page, then by default the right
       //    page should show the most recently created entry.
       // 2. If there is only one page, then the left page should show
       //    the most recently created entry and right page should be
       //    blank (hence need to return empty string).
-      if (defaultEntries.length !== 1 || position !== 'right') {
-        return defaultEntries[0]._id;
+      if (inputEntries.length !== 1 || position !== 'right') {
+        return inputEntries[0]._id;
       }
     }
 
@@ -118,7 +119,6 @@ class Page extends Component {
     } = this.props;
 
     const displayedEntryId = this.getDisplayedEntryId(entryId);
-    // const filteredEntries = this.filterEntries(entries);
 
     if (!displayedEntryId) {
       return <div className={this.getStyling()} />;
